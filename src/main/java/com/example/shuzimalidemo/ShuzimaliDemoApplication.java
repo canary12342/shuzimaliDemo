6dd46fba160be28e1c88fe549e1e091232ac235a
tx <11441990222@qq.com>
2025-06-13 00:26:43 +0800

    tanxin-6.13 demo基础架构构建

---
+++ src/main/java/com/example/shuzimalidemo/ShuzimaliDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.shuzimalidemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShuzimaliDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShuzimaliDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/shuzimalidemo/ShuzimaliDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.shuzimalidemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShuzimaliDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
