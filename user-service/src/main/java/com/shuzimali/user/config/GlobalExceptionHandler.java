26ba65a5b96476ac083eb0ae92aa02672c0f3ea5
tx <11441990222@qq.com>
2025-06-22 15:00:56 +0800

    tanxin-6.19-21 完成所有功能

---
+++ user-service/src/main/java/com/shuzimali/user/config/GlobalExceptionHandler.java
@@ -0,0 +1,25 @@
+package com.shuzimali.user.config;
+
+import com.shuzimali.common.exception.BusinessException;
+import com.shuzimali.user.entity.Result;
+import com.shuzimali.user.utils.ErrorCode;
+import com.shuzimali.user.utils.ResultUtils;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+@Slf4j
+public class GlobalExceptionHandler {
+    @ExceptionHandler(BusinessException.class)
+    public Result<?> businessExceptionHandler(BusinessException e) {
+        log.error("BusinessException", e);
+        return ResultUtils.error(e.getCode(), e.getMessage());
+    }
+
+    @ExceptionHandler(RuntimeException.class)
+    public Result<?> businessExceptionHandler(RuntimeException e) {
+        log.error("RuntimeException", e);
+        return ResultUtils.error(ErrorCode.SYSTEM_ERROR, "系统错误");
+    }
+}
